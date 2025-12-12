8c3abb35b237b209f1d212667449eee6223461bb
Ga1o <ga1ogramma@yandex.ru>
2025-12-12 21:19:05 +0300

    add test user model

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import UserModel
 
-# Create your tests here.
+
+class UserTest(TestCase):
+
+    def setUp(self):
+        # создание тестовйх данных
+        UserModel.objects.create(user_name='User 1')
+        UserModel.objects.create(user_name='User 2')
+
+    def test_user(self):
+        # тест создания пользователя
+        user = UserModel.objects.get(user_name='User 1')
+        self.assertEqual(user.user_name, 'User 1')
+
+    def test_user_count(self):
+        # тест количества пользователей
+        count = UserModel.objects.count()
+        self.assertEqual(count, 2)
